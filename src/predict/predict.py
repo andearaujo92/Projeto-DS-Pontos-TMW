4bcefa69c78e2a67d05507a70c9237086bdf0e4b
Anderson <ander.araujo92@gmail.com>
2025-04-20 23:27:13 -0300

    fix: ajustes em predict.py e abt.sql

--- src/predict/predict.py
+++ src/predict/predict.py
@@ -1,5 +1,7 @@
 #%%
 import pandas as pd
+import sqlalchemy
+from sqlalchemy import exc
 
 model_series = pd.read_pickle("../../models/model_rf.pkl")
 model_series
@@ -14,3 +16,27 @@ with open("etl.sql", "r") as open_file:
 df = pd.read_sql(query, engine)
 df
 # %%
+# Predições de Deploy
+preds = model_series['model'].predict(df[model_series['features']])
+proba_churn = model_series['model'].predict_proba(df[model_series['features']])[:,1]
+
+df_predict = pd.DataFrame(data = {
+    'dtRef': df['dtref'],
+    'idCustomer':df['idCustomer'],
+    'proba_churn':proba_churn
+})
+
+df_predict.sort_values('proba_churn', ascending = False)
+
+#%%
+# Ingestão no Banco
+with engine.connect() as con:
+
+    state = sqlalchemy.text(f"DELETE FROM tb_churn WHERE dtref = {df_predict['dtRef']}")
+    try:
+        con.execute(state)
+        con.commit()
+    except exc.OperationalError as e:
+        print("Tabela não encontrada... Criando Tabela")
+
+df_predict.to_sql("tb_churn", engine, if_exists='append', index = False)
